55d7370eeea91c98b06439ddc95b257bead43116
Janify <107705505+JanifyPremium@users.noreply.github.com>
2024-04-02 14:16:08 +0200

    anfang
    erstellen 3 funcs js main.html und stylesheet

---
+++ app.js
@@ -0,0 +1,39 @@
+/*function diplay(val){
+
+    document.getElementById('result').value += val
+
+    return val;
+}*/
+
+function solve() {
+    let x = document.getElementById('result').value;
+    let y = eval(x);
+
+    document.getElementById('result').value = y;
+    return y;
+}
+
+function clearScreen(){
+    document.getElementById('result').value = '';
+}
+
+let currentDisplay = "0"; 
+let resultDisplay = false; 
+
+function display(value) {
+    if (currentDisplay === "0" || resultDisplay) {
+      currentDisplay = value;
+    } else {
+      currentDisplay += value;
+    }
+  
+    resultDisplay = false;
+  
+    // Update the calculator display to show the new content
+    updateDisplay();
+  }
+  // Function to update the calculator display with the current content
+function updateDisplay() {
+    const displayElement = document.getElementById("display");
+    displayElement.textContent = currentDisplay;
+  }
