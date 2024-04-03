f8980aeea7ebc5a9f0e0131cf7b47680b45b8472
Janify <107705505+JanifyPremium@users.noreply.github.com>
2024-04-03 14:27:43 +0200

    kleine ausbesserung Rechtschreibfehler

--- JavaScriptcalc/app.js
+++ JavaScriptcalc/app.js
@@ -1,12 +1,10 @@
-//document.getElementById('result').addEventListener('click', display);
-//import
+
 const button = document.getElementById('result');
 button.addEventListener('click', function diplay(val){
 
     let a = document.getElementById('result').value += val;
 
     return val;
-    console.log(val);
 });
 
 button.addEventListener('click',function solve() {
