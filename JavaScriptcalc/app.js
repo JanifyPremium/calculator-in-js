2aa1ea590e388b09013c1ffee1d9c6ce51c7cd35
Janify <107705505+JanifyPremium@users.noreply.github.com>
2024-04-03 07:21:17 +0200

    added evventlistener

--- JavaScriptcalc/app.js
+++ JavaScriptcalc/app.js
@@ -1,21 +1,22 @@
 //document.getElementById('result').addEventListener('click', display);
 //import
-function diplay(){
+const button = document.getElementById('result')
+button.addEventListener('click', function diplay(){
 
     document.getElementById('result').value += val
 
     return val;
-}
+});
 
-function solve() {
+button.addEventListener('click',function solve() {
     let x = document.getElementById('result').value ;
     let y = eval(x);
 
     document.getElementById('result').value = y;
     return y;
-}
+});
 
-function clearScreen(){
+button.addEventListener('click',function clearScreen(){
     document.getElementById('result').value = '';
-}
+});
 
