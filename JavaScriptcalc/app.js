953fb47a9be302695ed83ab2a7e4567a919af3eb
Janify <107705505+JanifyPremium@users.noreply.github.com>
2024-04-04 07:47:03 +0200

    opimization

--- JavaScriptcalc/app.js
+++ JavaScriptcalc/app.js
@@ -2,21 +2,21 @@
 const button = document.getElementById('result').value;
 button.addEventListener('click', function diplay(num){
 
-    let a = document.getElementById('result').value += num
+    let a = button.value += num
 
     return num;
     console.log(num)
 });
 
 button.addEventListener('click',function solve() {
-    let x = document.getElementById('result').value ;
+    let x = button.value ;
     let y = eval(x);
 
-    document.getElementById('result').value = y;
+    button.value = y;
     return y;
 });
 
 button.addEventListener('click',function clearScreen(){
-    document.getElementById('result').value = '';
+    button.value = '';
 });
 
