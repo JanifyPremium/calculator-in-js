2e9d2093f4c36206e80c1c0deb0e0f40c8143cd4
Janify <107705505+JanifyPremium@users.noreply.github.com>
2024-04-05 08:52:55 +0200

    commented code

--- JavaScriptcalc/app.js
+++ JavaScriptcalc/app.js
@@ -1,5 +1,7 @@
 
 const button = document.getElementById('result').value;
+
+// Adds an event listener to the button for displaying the number
 button.addEventListener('click', function diplay(num){
 
     let a = button.value += num
@@ -7,6 +9,7 @@ button.addEventListener('click', function diplay(num){
     return num;
 });
 
+// Adds an event listener to the button for solving the expression
 button.addEventListener('click',function solve() {
     let x = button.value ;
     let y = eval(x);
@@ -15,6 +18,7 @@ button.addEventListener('click',function solve() {
     return y;
 });
 
+// Adds an event listener to the button for clearing the screen
 button.addEventListener('click',function clearScreen(){
     button.value = '';
 });
